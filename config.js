73106c72069c7f50ba59ea2c6ee03e58657c0f33
Boss <105100286+BossVarintorn@users.noreply.github.com>
2025-03-08 13:02:17 +0000

    changeScriptEXE

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 export default {
-    aprUrl: "https://script.google.com/macros/s/AKfycbxVUinvWzsp178oiD7ldBJFlc3J-Bke1377PsJ9uuBIbrKgI-TlxuO0xtzmqSV3af0b/exec"
+    aprUrl: "https://script.google.com/macros/s/AKfycbwtTBLMGiUEIT_xqNvZhMZ7Q_bsuH0T-o4GLxNTzm1atzQupMA-VDCc2YbTsRshYP-KsA/exec"
 }
